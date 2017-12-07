27dce2994eac40232f68f34343f06db5d22e82f9
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-07 12:46:37 +0000

    better dimensions on metrics

--- reporthealth.sh
+++ reporthealth.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 INSTANCE_ID=$(ec2metadata --instance-id)
 FAILURES=$(curl -s localhost:8080 | grep -c fail)
 SUCCESSES=$(curl -s localhost:8080 | grep -c true)
 CHECKS=$(curl -s localhost:8080 | grep -c "Connect to")
-
 REGION=$(ec2metadata --availability-zone | head -c-2)
 
-aws --region=${REGION} cloudwatch put-metric-data --metric-name ConnectivityTesterFailures --namespace ConnectivityTester --value ${FAILURES} --dimensions InstanceId=${INSTANCE_ID}
-aws --region=${REGION} cloudwatch put-metric-data --metric-name ConnectivityTesterSuccesses --namespace ConnectivityTester --value ${SUCCESSES} --dimensions InstanceId=${INSTANCE_ID}
-aws --region=${REGION} cloudwatch put-metric-data --metric-name ConnectivityTesterChecks --namespace ConnectivityTester --value ${CHECKS} --dimensions InstanceId=${INSTANCE_ID}
+function get_tag {
+    echo $(aws --region=$REGION ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" "Name=key,Values=${1}") | python -c $'import json,sys;obj=json.load(sys.stdin);\nif len(obj["Tags"])>0:\n print obj["Tags"][0]["Value"]\nelse:\n print "unknown"'
+}
+
+DIMENSIONS=Overall=overall,InstanceId=${INSTANCE_ID:-unknown},Name=$(get_tag Name),Service=$(get_tag Service),Environment=$(get_tag Environment),EnvironmentGroup=$(get_tag EnvironmentGroup)
+
+aws --region=${REGION} cloudwatch put-metric-data --dimensions ${DIMENSIONS} --metric-name ConnectivityTesterFail --namespace ConnectivityTester --value ${FAILURES}
+aws --region=${REGION} cloudwatch put-metric-data --dimensions ${DIMENSIONS} --metric-name ConnectivityTesterPass --namespace ConnectivityTester --value ${SUCCESSES}
+aws --region=${REGION} cloudwatch put-metric-data --dimensions ${DIMENSIONS} --metric-name ConnectivityTesterTest --namespace ConnectivityTester --value ${CHECKS}
 
