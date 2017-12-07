afed007b8d6f916809c2d1ef71769e5ac883ad00
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-07 16:33:39 +0000

    remove unnecessary/dupe metric dimensions

--- reporthealth.sh
+++ reporthealth.sh
@@ -9,7 +9,7 @@ function get_tag {
     echo $(aws --region=$REGION ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" "Name=key,Values=${1}") | python -c $'import json,sys;obj=json.load(sys.stdin);\nif len(obj["Tags"])>0:\n print obj["Tags"][0]["Value"]\nelse:\n print "unknown"'
 }
 
-DIMENSIONS=Overall=overall,InstanceId=${INSTANCE_ID:-unknown},Name=$(get_tag Name),Service=$(get_tag Service),Environment=$(get_tag Environment),EnvironmentGroup=$(get_tag EnvironmentGroup)
+DIMENSIONS=InstanceId=${INSTANCE_ID:-unknown},Service=$(get_tag Service),Environment=$(get_tag Environment),EnvironmentGroup=$(get_tag EnvironmentGroup)
 
 aws --region=${REGION} cloudwatch put-metric-data --dimensions ${DIMENSIONS} --metric-name ConnectivityTesterFail --namespace ConnectivityTester --value ${FAILURES}
 aws --region=${REGION} cloudwatch put-metric-data --dimensions ${DIMENSIONS} --metric-name ConnectivityTesterPass --namespace ConnectivityTester --value ${SUCCESSES}
