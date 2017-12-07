3da1dfdfbf5a27bebcb669e64ffe03ebe92a759f
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-07 12:46:24 +0000

    better region detection

--- startcloudwatchlogs.sh
+++ startcloudwatchlogs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Get the instance region and inject it in the conf
-REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.\{1\}$//')
+REGION=$(ec2metadata --availability-zone | head -c-2)
 sed -i -e 's/.*region.*/region = '$REGION'/' /var/awslogs/etc/aws.conf
 
 # Restart the awslogs agent
